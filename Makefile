cad869e7207fb3e39d0f07861b997e9f278eb173
David Lechner <david@lechnology.com>
2014-01-08 18:21:11 -0600

    legoev3-bluetooth: change bt_clock driver to complete bluetooth driver

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # Makefile for LEGO Mindstorms EV3 device drivers
 
 obj-$(CONFIG_LEGOEV3_ANALOG)		+= legoev3_analog.o
-obj-$(CONFIG_LEGOEV3_BT_CLOCK)		+= legoev3_bt_clock.o
+obj-$(CONFIG_LEGOEV3_BLUETOOTH)		+= legoev3_bluetooth.o
 obj-$(CONFIG_LEGOEV3_DEV_PORTS)		+= legoev3_ports.o
 obj-$(CONFIG_LEGOEV3_INPUT_PORT)	+= legoev3_input_port.o
 obj-$(CONFIG_LEGOEV3_TOUCH_SENSORS)	+= legoev3_touch_sensor_class.o
---
+++ legoev3_bluetooth.c
@@ -0,0 +1,157 @@
+/*
+ * On-board bluetooth support for LEGO Mindstorms EV3
+ *
+ * Copyright (C) 2013 David Lechner <david@lechnology.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/*
+ * -----------------------------------------------------------------------------
+ * The bluetooth module in the LEGO Mindstorms EV3 is a Panasonic
+ * PAN1325A-HCI-85, which has a TI CC2560A chip.
+ *
+ * The chip requires an external clock to function. This clock is generated
+ * by one of the PWM devices on the AM1808 SoC.
+ * -----------------------------------------------------------------------------
+ */
+
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/err.h>
+#include <linux/slab.h>
+#include <linux/platform_device.h>
+#include <linux/gpio.h>
+#include <linux/pwm/pwm.h>
+#include <linux/legoev3/legoev3_bluetooth.h>
+
+enum legoev3_bluetooth_gpios {
+	LEGOEV3_BT_GPIO_BT_ENA,
+	LEGOEV3_BT_GPIO_PIC_ENA,
+	LEGOEV3_BT_GPIO_PIC_RST,
+	LEGOEV3_BT_GPIO_PIC_CTS,
+	NUM_LEGOEV3_BT_GPIO
+};
+
+struct legoev3_bluetooth_device {
+	struct gpio gpios[NUM_LEGOEV3_BT_GPIO];
+	struct pwm_device *pwm;
+};
+
+/* TODO: add attributes to control device */
+
+static int __devinit legoev3_bluetooth_probe(struct platform_device *pdev)
+{
+	struct legoev3_bluetooth_device *btdev;
+	struct legoev3_bluetooth_platform_data *pdata;
+	struct pwm_device *pwm;
+	int err;
+
+	if (!pdev->dev.platform_data) {
+		dev_err(&pdev->dev, "%s: Platform data is required!\n", __func__);
+		return -EINVAL;
+	}
+	pdata = pdev->dev.platform_data;
+
+	btdev = kzalloc(sizeof(struct legoev3_bluetooth_device), GFP_KERNEL);
+	if (!btdev) {
+		dev_err(&pdev->dev, "%s: Could not allocate memory!\n",
+			__func__);
+		return -ENOMEM;
+	}
+
+	btdev->gpios[LEGOEV3_BT_GPIO_BT_ENA].gpio	= pdata->bt_ena_gpio;
+	btdev->gpios[LEGOEV3_BT_GPIO_BT_ENA].flags	= GPIOF_OUT_INIT_LOW;
+	btdev->gpios[LEGOEV3_BT_GPIO_BT_ENA].label	= "bluetooth enable";
+	btdev->gpios[LEGOEV3_BT_GPIO_PIC_ENA].gpio	= pdata->pic_ena_gpio;
+	btdev->gpios[LEGOEV3_BT_GPIO_PIC_ENA].flags	= GPIOF_OUT_INIT_LOW;
+	btdev->gpios[LEGOEV3_BT_GPIO_PIC_ENA].label	= "bt pic enable";
+	btdev->gpios[LEGOEV3_BT_GPIO_PIC_RST].gpio	= pdata->pic_rst_gpio;
+	btdev->gpios[LEGOEV3_BT_GPIO_PIC_RST].flags	= GPIOF_OUT_INIT_HIGH;
+	btdev->gpios[LEGOEV3_BT_GPIO_PIC_RST].label	= "bt pic reset";
+	btdev->gpios[LEGOEV3_BT_GPIO_PIC_CTS].gpio	= pdata->pic_cts_gpio;
+	btdev->gpios[LEGOEV3_BT_GPIO_PIC_CTS].flags	= GPIOF_IN;
+	btdev->gpios[LEGOEV3_BT_GPIO_PIC_CTS].label	= "bt pic cts";
+	err = gpio_request_array(btdev->gpios, NUM_LEGOEV3_BT_GPIO);
+	if (err) {
+		dev_err(&pdev->dev, "%s: Failed to request gpios! (%d)\n",
+			__func__, err);
+		goto err_gpio_request_array;
+	}
+
+	pwm = pwm_request_byname(pdata->clk_pwm_dev, "legoev3 bluetooth clock");
+	if (IS_ERR(pwm)) {
+		dev_err(&pdev->dev, "%s: Could not request pwm device '%s'! (%ld)\n",
+			__func__, pdata->clk_pwm_dev, PTR_ERR(pwm));
+		err = PTR_ERR(pwm);
+		goto err_pwm_request_byname;
+	}
+	err = pwm_set_frequency(pwm, 32768);
+	if (err) {
+		dev_err(&pdev->dev, "%s: Failed to set pwm frequency! (%d)\n",
+			__func__, err);
+		goto err_pwm_set_period_ticks;
+	}
+	err = pwm_set_duty_percent(pwm, 50);
+	if (err) {
+		dev_err(&pdev->dev, "%s: Failed to set pwm duty percent! (%d)\n",
+			__func__, err);
+		goto err_pwm_set_duty_ticks;
+	}
+	err = pwm_start(pwm);
+	if (err) {
+		dev_err(&pdev->dev, "%s: Failed to start pwm! (%d)\n",
+			__func__, err);
+		goto err_pwm_start;
+	}
+	gpio_set_value(btdev->gpios[LEGOEV3_BT_GPIO_BT_ENA].gpio, 1); /* active low */
+
+	platform_set_drvdata(pdev, btdev);
+
+	return 0;
+
+err_pwm_set_duty_ticks:
+err_pwm_set_period_ticks:
+err_pwm_start:
+	pwm_release(pwm);
+err_pwm_request_byname:
+	gpio_free_array(btdev->gpios, NUM_LEGOEV3_BT_GPIO);
+err_gpio_request_array:
+	kfree(btdev);
+	return err;
+}
+
+static int __devexit legoev3_bluetooth_remove(struct platform_device *pdev)
+{
+	struct legoev3_bluetooth_device *btdev = platform_get_drvdata(pdev);
+
+	pwm_release(btdev->pwm);
+	/* TODO: set gpios to turn off device */
+	gpio_free_array(btdev->gpios, NUM_LEGOEV3_BT_GPIO);
+	platform_set_drvdata(pdev, NULL);
+	kfree(btdev);
+
+	return 0;
+}
+
+struct platform_driver legoev3_bluetooth_driver = {
+	.probe = legoev3_bluetooth_probe,
+	.remove = __devexit_p(legoev3_bluetooth_remove),
+	.driver = {
+		.name = "legoev3-bluetooth",
+		.owner = THIS_MODULE,
+	},
+};
+module_platform_driver(legoev3_bluetooth_driver);
+
+MODULE_DESCRIPTION("Bluetooth driver for LEGO Mindstorms EV3");
+MODULE_AUTHOR("David Lechner <david@lechnology.com>");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:legoev3-bluetooth");
