6b6f410a818022f18dc9c60dfd87473958f4edaf
David Lechner <david@lechnology.com>
2016-02-15 18:11:57 -0600

    [travis] don't check push to master branch

--- checkpatch.sh
+++ checkpatch.sh
@@ -13,8 +13,13 @@ else
 fi
 
 if [ -z "$TRAVIS_REPO_SLUG" ]; then
-    git diff master | $check_patch --no-tree --no-signoff -
+    git diff origin/master | $check_patch --no-tree --no-signoff -
+    echo "OK!"
 else
+    if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+        # it is too late to check now.
+        exit 0
+    fi
     curl -i "https://github.com/$TRAVIS_REPO_SLUG/compare/ev3dev:master...$TRAVIS_COMMIT.diff" \
         | $check_patch --no-tree --no-signoff -
 fi
