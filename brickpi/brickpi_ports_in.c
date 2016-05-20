1c1375cb3d77c87a85de177e1abb6e4e03e36a12
Dan Kortschak <dan.kortschak@adelaide.edu.au>
2016-05-20 12:11:46 +0930

    Clarify need for address with NXT/I2C set_device (#38)
    * Clarify need for address with NXT/I2C set_device
    
    * Add note for generated port address
    
    * Add link to BrickPi I2C page

--- brickpi/brickpi_ports_in.c
+++ brickpi/brickpi_ports_in.c
@@ -90,8 +90,15 @@ static const struct lego_port_mode_info brickpi_in_port_mode_info[NUM_BRICKPI_IN
 	[BRICKPI_IN_PORT_MODE_NXT_I2C] = {
 		/**
 		 * [^nxt-i2c-mode]: No sensors are loaded by default. You must
-		 * manually specify the sensor that is connected by using the
-		 * `set_device` attribute.
+		 * manually specify the sensor that is connected and its address
+		 * by using the `set_device` attribute. This is equivalent to
+		 * [manually loading] I2C devices. The sensor port address will
+		 * be the BrickPi port address with ":i2c" and the decimal I2C
+		 * address appended. The [BrickPi I2C sensor page] has information
+		 * regarding the use of sensors connected to a brickpi-in-port.
+		 * ^
+		 * [manually loading]: /docs/sensors/using-i2c-sensors/#manually-loading-devices
+		 * [BrickPi I2C sensor]: /docs/drivers/brickpi-i2c-sensor/
 		 *
 		 * @description: NXT/I2C sensor
 		 * @name_footnote: [^nxt-i2c-mode]
