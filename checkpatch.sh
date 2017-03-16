bfb57d11fa834c494fec0e39f364b36636b6b4d6
David Lechner <david@lechnology.com>
2017-03-16 12:08:35 -0500

    ev3dev-jessie is the new default branch on GitHub.

--- checkpatch.sh
+++ checkpatch.sh
@@ -17,14 +17,14 @@ if [ ! -f "spelling.txt" ]; then
     fi
 
 if [ -z "$TRAVIS_REPO_SLUG" ]; then
-    git diff origin/master | $check_patch --no-tree --no-signoff -
+    git diff origin/ev3dev-jessie | $check_patch --no-tree --no-signoff -
     echo "OK!"
 else
-    if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+    if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "ev3dev-jessie" ]; then
         # it is too late to check now.
         exit 0
     fi
-    curl -i "https://github.com/$TRAVIS_REPO_SLUG/compare/ev3dev:master...$TRAVIS_COMMIT.diff" \
+    curl -i "https://github.com/$TRAVIS_REPO_SLUG/compare/ev3dev:ev3dev-jessie...$TRAVIS_COMMIT.diff" \
         | grep -v '\(^X-Served-By:\|^X-Request-Id:\|^Set-Cookie:\|^Content-Security-Policy:\|^Public-Key-Pins:\)' \
         | $check_patch --no-tree --no-signoff --ignore BAD_SIGN_OFF --ignore FILE_PATH_CHANGES -
 fi
