449bd92e10181e06ae0eea7ee9ab5da227ef3dbd
David Lechner <david@lechnology.com>
2018-06-21 11:20:23 -0500

    rproc: register rproc device for both PRUs

--- linux/rproc/da8xx_pru_rproc.c
+++ linux/rproc/da8xx_pru_rproc.c
@@ -6,26 +6,16 @@
  */
 
 #include <linux/bitops.h>
-#include <linux/debugfs.h>
 #include <linux/module.h>
+#include <linux/of_device.h>
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
 #include <linux/remoteproc.h>
 #include <linux/regmap.h>
+#include <linux/sizes.h>
+#include <linux/types.h>
 
-/* memory map */
-#define DA8XX_PRU_DATA_RAM_0	0x0000
-#define DA8XX_PRU_DATA_RAM_1	0x0200
-#define DA8XX_PRU_INTC		0x4000
-#define DA8XX_PRU_PRU0_CS	0x7000
-#define DA8XX_PRU_PRU1_CS	0x7800
-#define DA8XX_PRU_PRU0_INST	0x8000
-#define DA8XX_PRU_PRU1_INST	0xc000
-
-#define DA8XX_PRU_DATA_RAM_SIZE	0x200
-#define DA8XX_PRU_INTC_SIZE	0x3000
-#define DA8XX_PRU_CS_SIZE	0x800
-#define DA8XX_PRU_INST_SIZE	0x1000
+#define SZ_12K 0x3000
 
 /* control/status registers */
 #define DA8XX_PRU_CS_CONTROL	0x0
@@ -45,6 +35,18 @@
 /* status bits */
 #define DA8XX_PRU_STATUS_PCOUNTER	GENMASK(15, 0)
 
+enum ti_pru {
+	TI_PRU0,
+	TI_PRU1,
+	NUM_TI_PRU
+};
+
+enum ti_pru_type {
+	TI_PRU_TYPE_AM18XX,
+	TI_PRU_TYPE_AM335X,
+	NUM_TI_PRU_TYPE
+};
+
 enum da8xx_pru_evtout {
 	DA8XX_PRU_EVTOUT0,
 	DA8XX_PRU_EVTOUT1,
@@ -57,156 +59,296 @@ enum da8xx_pru_evtout {
 	NUM_DA8XX_PRU_EVTOUT
 };
 
-struct da8xx_pru_data {
+struct ti_pru_mem_region {
+	off_t offset;
+	size_t size;
+};
+
+/**
+ * ti_pru_shared_info - common init info for the PRUSS
+ * @ram: shared RAM, if present
+ * @intc: interrupt controller
+ */
+struct ti_pru_shared_info {
+	struct ti_pru_mem_region ram;
+	struct ti_pru_mem_region intc;
+};
+
+/**
+ * ti_pru_info - init info each individual PRU
+ * @ram: PRU RAM
+ * @ctrl: PRU control/status registers
+ * @dbg: PRU dbg registers
+ * @inst: instruction RAM
+ */
+struct ti_pru_info {
+	struct ti_pru_mem_region ram;
+	struct ti_pru_mem_region ctrl;
+	struct ti_pru_mem_region dbg;
+	struct ti_pru_mem_region inst;
+};
+
+struct ti_pru_device_info {
+	struct ti_pru_shared_info shared;
+	struct ti_pru_info pru[NUM_TI_PRU];
+};
+
+static const struct ti_pru_device_info ti_pru_devices[NUM_TI_PRU_TYPE] = {
+	[TI_PRU_TYPE_AM18XX] = {
+		.shared = {
+			.intc =	{ .offset = 0x4000,	.size = SZ_12K,	},
+		},
+		.pru[TI_PRU0] = {
+			.ram =	{ .offset = 0x0000,	.size = SZ_512,	},
+			.ctrl =	{ .offset = 0x7000,	.size = SZ_1K,	},
+			.dbg =	{ .offset = 0x7400,	.size = SZ_1K,	},
+			.inst =	{ .offset = 0x8000,	.size = SZ_4K,	},
+		},
+		.pru[TI_PRU1] = {
+			.ram =	{ .offset = 0x2000,	.size = SZ_512,	},
+			.ctrl =	{ .offset = 0x7800,	.size = SZ_1K,	},
+			.dbg =	{ .offset = 0x7c00,	.size = SZ_1K,	},
+			.inst =	{ .offset = 0xc000,	.size = SZ_4K,	},
+		},
+	},
+	[TI_PRU_TYPE_AM335X] = {
+		.shared = {
+			.ram =	{ .offset = 0x10000,	.size = SZ_12K,	},
+			.intc =	{ .offset = 0x20000,	.size = SZ_8K,	},
+		},
+		.pru[TI_PRU0] = {
+			.ram =	{ .offset = 0x00000,	.size = SZ_8K,	},
+			.ctrl =	{ .offset = 0x22000,	.size = SZ_1K,	},
+			.dbg =	{ .offset = 0x22400,	.size = SZ_1K,	},
+			.inst =	{ .offset = 0x34000,	.size = SZ_8K,	},
+		},
+		.pru[TI_PRU1] = {
+			.ram =	{ .offset = 0x02000,	.size = SZ_8K,	},
+			.ctrl =	{ .offset = 0x24000,	.size = SZ_1K,	},
+			.dbg =	{ .offset = 0x24400,	.size = SZ_1K,	},
+			.inst =	{ .offset = 0x38000,	.size = SZ_8K,	},
+		},
+	},
+};
+
+struct ti_pru_shared_data {
+	const struct ti_pru_shared_info *info;
 	struct device *dev;
 	void __iomem *base;
-	struct rproc *rproc;
-	struct regmap *regmap;
 	int evtout_irq[NUM_DA8XX_PRU_EVTOUT];
+	struct rproc *pru[NUM_TI_PRU];
+};
+
+struct ti_pru_data {
+	const struct ti_pru_info *info;
+	struct ti_pru_shared_data *shared;
+	struct regmap *ctrl;
 };
 
 static int da8xx_pru_rproc_start(struct rproc *rproc)
 {
-	struct da8xx_pru_data *data = rproc->priv;
+	struct ti_pru_data *pru = rproc->priv;
 	u32 val;
 
 	val = (rproc->bootaddr >> 2) << (ffs(DA8XX_PRU_CONTROL_PCRESETVAL) - 1);
 	val |= DA8XX_PRU_CONTROL_ENABLE;
 
-	return regmap_write(data->regmap, DA8XX_PRU_CS_CONTROL, val);
+	return regmap_write(pru->ctrl, DA8XX_PRU_CS_CONTROL, val);
 }
 
 static int da8xx_pru_rproc_stop(struct rproc *rproc)
 {
-	struct da8xx_pru_data *data = rproc->priv;
+	struct ti_pru_data *pru = rproc->priv;
 	u32 mask;
 
 	mask = DA8XX_PRU_CONTROL_ENABLE;
 
-	return regmap_write_bits(data->regmap, DA8XX_PRU_CS_CONTROL, mask, 0);
+	return regmap_write_bits(pru->ctrl, DA8XX_PRU_CS_CONTROL, mask, 0);
 }
 
 static void *da8xx_pru_rproc_da_to_va(struct rproc *rproc, u64 da, int len,
 				      int page)
 {
-	struct da8xx_pru_data *data = rproc->priv;
+	struct ti_pru_data *pru = rproc->priv;
+	struct ti_pru_shared_data *shared = pru->shared;
 
 	if (page == 0) {
-		if (da + len > DA8XX_PRU_INST_SIZE)
+		if (da + len > pru->info->inst.size)
 			return ERR_PTR(-EINVAL);
 
-		return data->base + DA8XX_PRU_PRU0_INST + da;
+		return shared->base + pru->info->inst.offset + da;
 	}
 
 	if (page == 1) {
-		if (da + len > DA8XX_PRU_DATA_RAM_SIZE)
+		if (da + len > pru->info->ram.size)
 			return ERR_PTR(-EINVAL);
 
-		return data->base + DA8XX_PRU_DATA_RAM_0 + da;
+		return shared->base + pru->info->ram.offset + da;
 	}
 
 	return ERR_PTR(-EINVAL);
 }
 
-static const struct rproc_ops da8xx_pru_rproc_ops = {
+static const struct rproc_ops ti_pru_rproc_ops = {
 	.start = da8xx_pru_rproc_start,
 	.stop = da8xx_pru_rproc_stop,
 	.da_to_va = da8xx_pru_rproc_da_to_va,
 };
 
-static struct regmap_config da8xx_pru_regmap_config = {
+static struct regmap_config ti_pru_ctrl_regmap_config = {
 	.reg_bits = 32,
 	.val_bits = 32,
 	.reg_stride = 4,
 	.max_register = 0x2c,
 };
 
+static const struct of_device_id ti_pru_rproc_of_match[] = {
+	{
+		.compatible = "ev3dev,da850-pru-rproc",
+		.data = &ti_pru_devices[TI_PRU_TYPE_AM18XX]
+	},
+	{
+		.compatible = "ev3dev,am3352-pru-rproc",
+		.data = &ti_pru_devices[TI_PRU_TYPE_AM335X]
+	},
+	{ },
+};
+MODULE_DEVICE_TABLE(of, ti_pru_rproc_of_match);
+
+static struct rproc *ti_pru_init_one_rproc(struct ti_pru_shared_data *shared,
+					   const struct ti_pru_info *info,
+					   enum ti_pru id)
+{
+	struct device *dev = shared->dev;
+	const char *name;
+	struct rproc *rproc;
+	struct ti_pru_data *pru;
+
+	name = devm_kasprintf(dev, GFP_KERNEL, "pru%u", id);
+	if (!name)
+		return ERR_PTR(-ENOMEM);
+
+	rproc = rproc_alloc(dev, name, &ti_pru_rproc_ops, NULL, sizeof(*pru));
+	if (!rproc)
+		return ERR_PTR(-ENOMEM);
+
+	/* don't auto-boot for now - bad firmware can lock up the system */
+	rproc->auto_boot = false;
+
+	pru = rproc->priv;
+	pru->info = info;
+	pru->shared = shared;
+
+	pru->ctrl = devm_regmap_init_mmio(&rproc->dev,
+					  shared->base + info->ctrl.offset,
+					  &ti_pru_ctrl_regmap_config);
+	if (IS_ERR(pru->ctrl)) {
+		dev_err(&rproc->dev, "failed to init regmap\n");
+		return ERR_CAST(pru->ctrl);
+	}
+
+	return rproc;
+}
+
 static int da8xx_pru_rproc_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	struct rproc *rproc;
-	struct da8xx_pru_data *data;
+	const struct of_device_id *of_id;
+	const struct ti_pru_device_info *info;
+	struct ti_pru_shared_data *shared;
 	struct resource *res;
-	int i, ret;
+	int i, err;
 
-	rproc = rproc_alloc(dev, pdev->name, &da8xx_pru_rproc_ops, "ev3-pru-test.out",
-			    sizeof(*data));
-	if (!rproc)
-		return -ENOMEM;
+	of_id = of_match_device(ti_pru_rproc_of_match, dev);
+	if (!of_id || !of_id->data)
+		return -EINVAL;
 
-	data = rproc->priv;
-	data->dev = dev;
-	data->rproc = rproc;
+	info = of_id->data;
 
-	platform_set_drvdata(pdev, data);
+	shared = devm_kzalloc(dev, sizeof(*shared), GFP_KERNEL);
+
+	platform_set_drvdata(pdev, shared);
+
+	shared->info = &info->shared;
+	shared->dev = dev;
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	data->base = devm_ioremap_resource(dev, res);
-	if (IS_ERR(data->base)) {
+	shared->base = devm_ioremap_resource(dev, res);
+	if (IS_ERR(shared->base)) {
 		dev_err(dev, "failed to ioremap resource\n");
-		return PTR_ERR(data->base);
-	}
-
-	data->regmap = devm_regmap_init_mmio(dev, data->base + DA8XX_PRU_PRU0_CS,
-					     &da8xx_pru_regmap_config);
-	if (IS_ERR(data->regmap)) {
-		dev_err(dev, "failed to initialize regmap\n");
-		return PTR_ERR(data->regmap);
+		return PTR_ERR(shared->base);
 	}
 
 	for (i = 0; i < NUM_DA8XX_PRU_EVTOUT; i++) {
-		data->evtout_irq[i] = platform_get_irq(pdev, i);
-		if (data->evtout_irq[i] < 0) {
+		shared->evtout_irq[i] = platform_get_irq(pdev, i);
+		if (shared->evtout_irq[i] < 0) {
 			dev_err(dev, "failed to get IRQ for EVTOUT%d\n", i);
-			return data->evtout_irq[i];
+			return shared->evtout_irq[i];
 		}
+	}
+
+	shared->pru[TI_PRU0] = ti_pru_init_one_rproc(shared, &info->pru[TI_PRU0],
+						     TI_PRU0);
+	if (IS_ERR(shared->pru[TI_PRU0]))
+		return PTR_ERR(shared->pru[TI_PRU0]);
+
+	shared->pru[TI_PRU1] = ti_pru_init_one_rproc(shared, &info->pru[TI_PRU1],
+						     TI_PRU1);
+	if (IS_ERR(shared->pru[TI_PRU1])) {
+		err = PTR_ERR(shared->pru[TI_PRU1]);
+		goto err_free_pru0;
 	}
 
 	pm_runtime_enable(dev);
 
-	ret = pm_runtime_get_sync(dev);
-	if (ret < 0)
+	err = pm_runtime_get_sync(dev);
+	if (err < 0)
 		goto err_pm_runtime_disable;
 
-	ret = rproc_add(rproc);
-	if (ret < 0)
+	err = rproc_add(shared->pru[TI_PRU0]);
+	if (err < 0)
 		goto err_pm_runtime_put;
+
+	err = rproc_add(shared->pru[TI_PRU1]);
+	if (err < 0)
+		goto err_del_pru0;
 
 	return 0;
 
+err_del_pru0:
+	rproc_del(shared->pru[TI_PRU0]);
 err_pm_runtime_put:
 	pm_runtime_put(dev);
 err_pm_runtime_disable:
 	pm_runtime_disable(dev);
+	rproc_free(shared->pru[TI_PRU1]);
+err_free_pru0:
+	rproc_free(shared->pru[TI_PRU0]);
 
-	return ret;
+	return err;
 }
 
 static int da8xx_pru_rproc_remove(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	struct da8xx_pru_data *data = platform_get_drvdata(pdev);
+	struct ti_pru_shared_data *shared = platform_get_drvdata(pdev);
 
-	rproc_del(data->rproc);
+	rproc_del(shared->pru[TI_PRU1]);
+	rproc_del(shared->pru[TI_PRU0]);
 	pm_runtime_put(dev);
 	pm_runtime_disable(dev);
-	rproc_free(data->rproc);
+	rproc_free(shared->pru[TI_PRU1]);
+	rproc_free(shared->pru[TI_PRU0]);
 
 	return 0;
 }
-
-static const struct of_device_id da8xx_pru_rproc_of_match[] = {
-	{ .compatible = "ev3dev,da850-pru-rproc", },
-	{ },
-};
-MODULE_DEVICE_TABLE(of, da8xx_pru_rproc_of_match);
 
 static struct platform_driver da8xx_pru_rproc_driver = {
 	.probe	= da8xx_pru_rproc_probe,
 	.remove	= da8xx_pru_rproc_remove,
 	.driver	= {
-		.name = "da8xx-pru-rproc",
-		.of_match_table = da8xx_pru_rproc_of_match,
+		.name = "ti-pru-rproc",
+		.of_match_table = ti_pru_rproc_of_match,
 	},
 };
 
@@ -214,4 +356,4 @@ module_platform_driver(da8xx_pru_rproc_driver);
 
 MODULE_AUTHOR("David Lechner <david@lechnology.com>");
 MODULE_LICENSE("GPL v2");
-MODULE_DESCRIPTION("Remoteproc driver for TI AM18xx/OMAPL138 PRU");
+MODULE_DESCRIPTION("Remoteproc driver for TI PRU");
