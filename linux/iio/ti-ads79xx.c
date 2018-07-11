56217b493225ab316933d0da3c0fec309508d956
David Lechner <david@lechnology.com>
2018-07-10 23:12:58 -0500

    ti-ads79xx: make use of iio_device_claim_direct_mode()

--- linux/iio/ti-ads79xx.c
+++ linux/iio/ti-ads79xx.c
@@ -322,12 +322,13 @@ static int ti_ads79xx_read_raw(struct iio_dev *indio_dev,
 
 	switch (m) {
 	case IIO_CHAN_INFO_RAW:
-		mutex_lock(&indio_dev->mlock);
-		if (iio_buffer_enabled(indio_dev))
-			ret = -EBUSY;
-		else
+		ret = iio_device_claim_direct_mode(indio_dev);
+		if (ret)
+			return ret;
+
 			ret = ti_ads79xx_scan_direct(st, chan->address);
-		mutex_unlock(&indio_dev->mlock);
+
+		iio_device_release_direct_mode(indio_dev);
 
 		if (ret < 0)
 			return ret;
