0ff32d9f55af99ff7f770f7dd764731ee40c372d
Eisverygoodletter <76079042+Eisverygoodletter@users.noreply.github.com>
2022-01-26 09:47:44 +0800

    changing HiTechnic IrSeeker V2 docs based on #1525 (#84)

--- sensors/nxt_i2c_sensor_defs.c
+++ sensors/nxt_i2c_sensor_defs.c
@@ -770,9 +770,16 @@ const struct nxt_i2c_sensor_info nxt_i2c_sensor_defs[] = {
 				 *     ...
 				 *     9        Far right
 				 *    =======  =============
-				 *
+				 *    AC mode runs the Sensor in modulated mode, aimed at detecting modulated IR signals such as from HiTechnic IRBall or
+				 *    some types of IR remote controls.
+				 *    DC mode runs the Sensor in unmodulated mode, detecting both modulated and unmodulated IR signals such as sunlight.
+				 *    These directions do not directly corresponds to the DC-ALL or AC-ALL mode's 5 sensor values. 
+				 *    the even number directions are calculated in the sensor, while the odd number directions
+				 *    are measured actually measured by sensors 1-5 inside the HiTechnic NXT IRSeeker V2
+				 * 
+				 * 
 				 * @description: Direction (unmodulated)
-				 * @value0: Direction (0 to 9)
+				 * @value0: Direction (0 to 9) 
 				 * @value0_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
 				 */
 				.name = "DC",
@@ -791,21 +798,24 @@ const struct nxt_i2c_sensor_info nxt_i2c_sensor_defs[] = {
 			},
 			[2] = {
 				/**
+				 * .. [#ht-nxt-ir-seek-v2-mode2-value1] Signal strengths may not reach as high as 255. Strength is dependent on many factors such as IR source strength etc.
+				 */
+				/**
 				 * @description: All values (unmodulated)
 				 * @value0: Direction (0 to 9)
 				 * @value0_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
-				 * @value1: Sensor 1 signal strength (0 to 9)
-				 * @value1_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
-				 * @value2: Sensor 2 signal strength (0 to 9)
-				 * @value2_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
-				 * @value3: Sensor 3 signal strength (0 to 9)
-				 * @value3_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
-				 * @value4: Sensor 4 signal strength (0 to 9)
-				 * @value4_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
-				 * @value5: Sensor 5 signal strength (0 to 9)
-				 * @value5_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
-				 * @value6: Sensor mean (0 to 9)
-				 * @value6_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
+				 * @value1: Sensor 1 (far left direction) signal strength (0-255)
+				 * @value1_footnote: [#ht-nxt-ir-seek-v2-mode2-value1]_
+				 * @value2: Sensor 2 (left-forward direction) signal strength (0-255)
+				 * @value2_footnote: [#ht-nxt-ir-seek-v2-mode2-value1]_
+				 * @value3: Sensor 3 (forward direction) signal strength (0-255)
+				 * @value3_footnote: [#ht-nxt-ir-seek-v2-mode2-value1]_
+				 * @value4: Sensor 4 (right-forward direction) signal strength (0-255)
+				 * @value4_footnote: [#ht-nxt-ir-seek-v2-mode2-value1]_
+				 * @value5: Sensor 5 (far right direction) signal strength (0-255)
+				 * @value5_footnote: [#ht-nxt-ir-seek-v2-mode2-value1]_
+				 * @value6: Sensor mean (0-255)
+				 * @value6_footnote: [#ht-nxt-ir-seek-v2-mode2-value1]_
 				 */
 				.name = "DC-ALL",
 				.data_sets = 7,
@@ -815,16 +825,16 @@ const struct nxt_i2c_sensor_info nxt_i2c_sensor_defs[] = {
 				 * @description: All values (modulated)
 				 * @value0: Direction (0 to 9)
 				 * @value0_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
-				 * @value1: Sensor 1 signal strength (0 to 9)
-				 * @value1_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
-				 * @value2: Sensor 2 signal strength (0 to 9)
-				 * @value2_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
-				 * @value3: Sensor 3 signal strength (0 to 9)
-				 * @value3_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
-				 * @value4: Sensor 4 signal strength (0 to 9)
-				 * @value4_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
-				 * @value5: Sensor 5 signal strength (0 to 9)
-				 * @value5_footnote: [#ht-nxt-ir-seek-v2-mode0-value0]_
+				 * @value1: Sensor 1 (far left direction) signal strength (0-255)
+				 * @value1_footnote: [#ht-nxt-ir-seek-v2-mode2-value1]_
+				 * @value2: Sensor 2 (left-forward direction) signal strength (0-255)
+				 * @value2_footnote: [#ht-nxt-ir-seek-v2-mode2-value1]_
+				 * @value3: Sensor 3 (forward direction) signal strength (0-255)
+				 * @value3_footnote: [#ht-nxt-ir-seek-v2-mode2-value1]_
+				 * @value4: Sensor 4 (right-forward direction) signal strength (0-255)
+				 * @value4_footnote: [#ht-nxt-ir-seek-v2-mode2-value1]_
+				 * @value5: Sensor 5 (far right direction) signal strength (0-255)
+				 * @value5_footnote: [#ht-nxt-ir-seek-v2-mode2-value1]_
 				 */
 				.name = "AC-ALL",
 				.data_sets = 6,
